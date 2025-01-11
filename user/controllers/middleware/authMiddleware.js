99c83050bc964c96ddda15b7ca1cb9b2f5e76a6f
pratik wayase <128498128+PratikWayase@users.noreply.github.com>
2025-01-11 15:03:18 +0530

    Add files via upload

---
+++ user/controllers/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.model');
+const blacklisttokenModel = require('../models/blacklisttoken.model')
+
+module.exports.userAuth = async ( req,res,next) => {
+    try{
+        const token = req.cookies.token || req.headers.autorization.split('')[1];
+        if  (!token){
+            return res.status(401).json({message:'unauthorized'})
+
+        }
+
+            const isBlacklisted = await blacklisttokenModel.find({token})
+
+            if (isBlacklisted.length){
+            return res.status(401).json({message:'unauthorized'})
+            }
+
+            const decode = jwt.verify(token,process.env.JWT_SECRET);
+            const user = await userModel.findById(decode.id)
+
+            if  (!user) {
+                return res.status(401).json({message:'unauthorized'})
+            }
+
+            req.user = user;
+            next ();
+
+        
+
+    }catch (error){
+        res.status(500).json({message:error.message})
+
+    }
+}
